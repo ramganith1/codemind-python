54f375181a3f6be567f0f41ffc58d2ffe00a1f60
ramganith1 <112338048+ramganith1@users.noreply.github.com>
2022-09-29 19:47:12 +0530

    n=int(input()) sum=0 while n>0:     r=n%10     sum+=r**2     n=n//10     if n==0 and sum>9:         n=sum         sum=0 if sum==1 or sum==7:     print('True') else:     print('False')

---
+++ happy_number.py
@@ -0,0 +1,13 @@
+n=int(input())
+sum=0
+while n>0:
+    r=n%10
+    sum+=r**2
+    n=n//10
+    if n==0 and sum>9:
+        n=sum
+        sum=0
+if sum==1 or sum==7:
+    print('True')
+else:
+    print('False')
